cf2758c0c01118db76b7f7764fe9684e648c0e1d
giobrtn <giovannibertini96@hotmail.com>
2023-10-20 17:36:28 -0300

    commit to create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerListComponent } from './pages/customer-list/customer-list.component';
+import { CustomerEditComponent } from './pages/customer-edit/customer-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customer-list', component: CustomerListComponent },
+  { path: 'customer-edit', component: CustomerEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CustomerListComponent } from './pages/customer-list/customer-list.component';
+import { CustomerEditComponent } from './pages/customer-edit/customer-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CustomerListComponent,
+    CustomerEditComponent
   ],
   imports: [
     BrowserModule,
